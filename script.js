8a03d550d5f78b65e22b941cba1bd8e8b7f06fbd
Gooten <Gootenmeister@users.noreply.github.com>
2024-09-23 10:20:59 +0200

    Rens för bygge

--- script.js
+++ script.js
@@ -1,21 +1,2 @@
-// JS kod goes here
-// todo: en fungerande html-sida med html och css innan man börjar
-// implementera JSkod enligt anvisningarna (finns i readme.md)
-
-// slänger upp en enkel print func
-const printText = (input) => {
-    const injectP0 = document.createElement('p')
-    const inputText0 = document.createTextNode(input)
-    injectP0.appendChild(inputText0)
-
-    //header0 är den första diven i <header> i body
-    const container0 = document.getElementById('header0')
-    container0.appendChild(injectP0)
-    container0.classList.add('class0')
-}
-
-const Obj = {
-    name: "user"
-}
-
-printText(`Tjena ${Obj.name}, hur är läget?`)
+// TODO: fungerande HTML som validerar innan JS och Vue.JS
+// AFTER: fixa en JSON fetch/promise med JS och live-reaktiv JS / Vue.JS som svarar på user input
