d71558c69c8ed38a9cb445716662fd75ea006d71
Gooten <Gootenmeister@users.noreply.github.com>
2024-09-27 14:37:31 +0200

    coolBeans :100:

--- Gustav/adminGustav.js
+++ Gustav/adminGustav.js
@@ -1,4 +1,30 @@
-//js goes here
-addEventListener("click", (event) => {
-    alert('jobbig click alert!')
-})
+//skillbarAnim
+document.getElementById('skills').addEventListener('click', function() {
+    const skillbars = document.getElementById('skillbars');
+    skillbars.classList.toggle('hidden');
+
+    const skillbarFills = document.querySelectorAll('.skillbar-fill');
+    const skillbarTitles = document.querySelectorAll('.skillbar-title');
+
+    if (!skillbars.classList.contains('hidden')) {
+        skillbarTitles.forEach(skillbarTitle => {
+            skillbarTitle.style.opacity = '1'; 
+        });
+        
+            //hol up
+        setTimeout(() => {
+            const widths = ['90%', '80%', '70%', '5%']; //vals
+            skillbarFills.forEach((skillbarFill, index) => {
+                skillbarFill.style.width = widths[index];
+            });
+        }, 300);
+    } else {
+        skillbarFills.forEach(skillbarFill => {
+            skillbarFill.style.width = '0'; //döljer skillbrs
+        });
+
+        skillbarTitles.forEach(skillbarTitle => {
+            skillbarTitle.style.opacity = '0';
+        });
+    }
+});
