43f730ec47d3b6fc4e1a18dea403e0c67b9ed1c6
Rikard Söderek <153732664+RikardS1@users.noreply.github.com>
2024-10-03 16:38:27 +0200

    typ klart bildspel

--- Rikard/adminRikard.js
+++ Rikard/adminRikard.js
@@ -1,3 +1,40 @@
+const imageCount = 7;
+const imageFolder = 'bildspel/';
+let currentIndex = 0;
+
+const imageElement = document.getElementById('picture-gallery-main');
+const prevButton = document.getElementById('prev-btn');
+const nextButton = document.getElementById('next-btn');
+
+
+function getImagePath(index) {
+    return `${imageFolder}bild${index + 1}.png`;
+}
+
+function updateImage() {
+    imageElement.src = getImagePath(currentIndex);
+}
+
+// Hantera föregående knapptryckning
+prevButton.addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + imageCount) % imageCount;
+    updateImage();
+});
+
+// Hantera nästa knapptryckning
+nextButton.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % imageCount; // Cirkulera till början
+    updateImage();
+});
+
+// Initial bildvisning
+updateImage();
+
+
+
+
+
+
 const app = Vue.createApp({
     data() {
         return {
